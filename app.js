dd99128ff7e83ad37d6c0570aa2848cd6dc7c02c
Macon Stokes <citostokes@gmail.com>
2017-07-12 08:41:19 -0500

    Mid Databases - end of Prepared Statements Section

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 var express = require('express');
 
 var app = express();
+var sql = require('mssql');
+var config = {
+  user: 'mstokes',
+  domain: 'ampcorp',
+  password: 'gr33ngiant',
+  server: 'am01sql003.ampcorp.org',
+  database: 'WarehouseMgt'
+};
+
+sql.connect(config, function (err) {
+  console.log(err);
+});
 
 var port = process.env.PORT || 5000;
 var nav = [{
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,50 +1,66 @@
 var express = require('express');
-
 var bookRouter = express.Router();
+var sql = require('mssql');
 
 var router = function(nav) {
   var books = [
-  {
-    title: 'War & Peace',
-    genre: 'Historical Fiction',
-    author: 'Lev Nikolayevich Tolstoy',
-    read: false
-  },
-  {
-    title: 'Les Miserables',
-    genre: 'Historical Fiction',
-    author: 'Victor Hugo',
-    read: false
-  },
-  {
-    title: 'A Journey into the Center of the Earth',
-    genre: 'Science Fiction',
-    author: 'Jules Verne',
-    read: false
-  }
-];
+    {
+      title: 'War & Peace',
+      genre: 'Historical Fiction',
+      author: 'Lev Nikolayevich Tolstoy',
+      read: false
+    },
+    {
+      title: 'Les Miserables',
+      genre: 'Historical Fiction',
+      author: 'Victor Hugo',
+      read: false
+    },
+    {
+      title: 'A Journey into the Center of the Earth',
+      genre: 'Science Fiction',
+      author: 'Jules Verne',
+      read: false
+    }
+  ];
 
   bookRouter.route('/')
     .get(function (req, res) {
-    res.render('bookListView', {
-      title: 'Books',
-      nav: nav,
-      books: books
-    });
-  });
+      var request = new sql.Request();
 
-  bookRouter.route('/:id')
+      request.query('select top 5 row_number() over (order by name) as [id], Name as [title], company as [author] from COMPANY where ACTIVE = \'Y\'',
+                    function(err, recordset) {
+                        //console.log(recordset.recordset);
+                        //console.log(books);
+                        res.render('bookListView', {
+                          title: 'Books',
+                          nav: nav,
+                          books: recordset.recordset
+                        });
+                      });
+
+    });
+
+  bookRouter.route('/:author')
     //this gives us whatever is beyond the ":"
     //so if a user enters /books/X this function will return X
     .get(function (req, res) {
-    var id = req.params.id;
-    //req.params.id gives us access to X and we are passing it to "id"
-    res.render('bookView', {
-      title: 'Books',
-      nav: nav,
-      book: books[id]
+      var author = req.params.author;
+      var ps = new sql.PreparedStatement();
+      ps.input('author', sql.NVarChar);
+      ps.prepare('select top 5 row_number() over (order by name) as [id], Name as [title], company as [author] from COMPANY where ACTIVE = \'Y\' and company = @author',
+        function(err) {
+          ps.execute({author:req.params.author},
+            function(err, recordset) {  //should this be recordset.recordset?  no pulling back the entire set
+              res.render('bookView', {
+                title: 'Books',
+                nav: nav,
+                book: recordset.recordset[0] // this is where we use recordset.recordset because we just want the inner recordset
+              });
+            });
+        });
+      //req.params.id gives us access to X and we are passing it to "id"
     });
-  });
 
   return bookRouter;
 };
