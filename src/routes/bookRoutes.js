abfb8753b16dcb16d9fa095bcc7f4022226a9a91
Macon Stokes <citostokes@gmail.com>
2017-07-18 08:22:34 -0500

    End of Middleware (using SQL as DB Engine)

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -3,26 +3,6 @@ var bookRouter = express.Router();
 var sql = require('mssql');
 
 var router = function(nav) {
-  var books = [
-    {
-      title: 'War & Peace',
-      genre: 'Historical Fiction',
-      author: 'Lev Nikolayevich Tolstoy',
-      read: false
-    },
-    {
-      title: 'Les Miserables',
-      genre: 'Historical Fiction',
-      author: 'Victor Hugo',
-      read: false
-    },
-    {
-      title: 'A Journey into the Center of the Earth',
-      genre: 'Science Fiction',
-      author: 'Jules Verne',
-      read: false
-    }
-  ];
 
   bookRouter.route('/')
     .get(function (req, res) {
@@ -44,22 +24,30 @@ var router = function(nav) {
   bookRouter.route('/:author')
     //this gives us whatever is beyond the ":"
     //so if a user enters /books/X this function will return X
-    .get(function (req, res) {
-      var author = req.params.author;
-      var ps = new sql.PreparedStatement();
-      ps.input('author', sql.NVarChar);
-      ps.prepare('select top 5 row_number() over (order by name) as [id], Name as [title], company as [author] from COMPANY where ACTIVE = \'Y\' and company = @author',
+    .all(function (req, res, next) {
+          var author = req.params.author;
+          var ps = new sql.PreparedStatement();
+          ps.input('author', sql.NVarChar);
+          ps.prepare('select top 5 row_number() over (order by name) as [id], Name as [title], company as [author] from COMPANY where ACTIVE = \'Y\' and company = @author',
         function(err) {
-          ps.execute({author:req.params.author},
+            ps.execute({author:req.params.author},
             function(err, recordset) {  //should this be recordset.recordset?  no pulling back the entire set
-              res.render('bookView', {
-                title: 'Books',
-                nav: nav,
-                book: recordset.recordset[0] // this is where we use recordset.recordset because we just want the inner recordset
-              });
+              if (recordset.recordset.length === 0) {
+                res.status(404).send('Not Found');
+              } else {
+                req.book = recordset.recordset[0]; // this is where we use recordset.recordset because we just want the inner recordset
+                next();
+              }
+
             });
-        });
-      //req.params.id gives us access to X and we are passing it to "id"
+          });
+        })
+    .get(function (req, res) {
+      res.render('bookView', {
+        title: 'Books',
+        nav: nav,
+        book: req.book
+      });
     });
 
   return bookRouter;
